aeaaec1d85f7fd0f9d11feb17cf732e46d94678f
hi-rustin <rustin.liu@gmail.com>
2020-11-01 16:20:59 +0800

    Impl sync

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,85 @@
 use std::cell::UnsafeCell;
+mod unsync {
+    use super::UnsafeCell;
+    pub struct OnceCell<T> {
+        inner: UnsafeCell<Option<T>>,
+    }
 
-pub struct OnceCell<T> {
-    inner: UnsafeCell<Option<T>>,
+    impl<T> OnceCell<T> {
+        pub fn new() -> Self {
+            Self {
+                inner: UnsafeCell::new(None),
+            }
+        }
+
+        pub fn get(&self) -> Option<&T> {
+            let ptr = self.inner.get();
+            // SAFETY:
+            // We're sure that the pointer is valid
+            // We're in a single thread and so no race condition is possible
+            // We're always returning a &T not a &mut T
+            unsafe { &*ptr }.as_ref()
+        }
+
+        pub fn set(&self, value: T) -> Result<(), T> {
+            if self.get().is_some() {
+                return Err(value);
+            }
+            // SAFETY:
+            // * we have exclusive access. We must write the value
+            let r = unsafe { &mut *self.inner.get() };
+            let old = std::mem::replace(r, Some(value));
+            debug_assert!(old.is_none());
+            Ok(())
+        }
+    }
 }
 
-impl<T> OnceCell<T> {
-    pub fn new() -> Self {
-        Self {
-            inner: UnsafeCell::new(None),
-        }
+mod sync {
+    use super::UnsafeCell;
+    use std::sync::Once;
+
+    pub struct OnceCell<T> {
+        inner: UnsafeCell<Option<T>>,
+        once: Once,
     }
 
-    pub fn get(&self) -> Option<&T> {
-        let ptr = self.inner.get();
-        // SAFETY:
-        // We're sure that the pointer is valid
-        // We're in a single thread and so no race condition is possible
-        // We're always returning a &T not a &mut T
-        unsafe { &*ptr }.as_ref()
-    }
+    unsafe impl<T> Sync for OnceCell<T> {}
 
-    pub fn set(&self, value: T) -> Result<(), T> {
-        if self.get().is_some() {
-            return Err(value);
+    impl<T> OnceCell<T> {
+        pub fn new() -> Self {
+            Self {
+                inner: UnsafeCell::new(None),
+                once: Once::new(),
+            }
         }
-        // SAFETY:
-        // * we have exclusive access. We must write the value
-        let r = unsafe { &mut *self.inner.get() };
-        let old = std::mem::replace(r, Some(value));
-        debug_assert!(old.is_none());
-        Ok(())
+
+        pub fn get(&self) -> Option<&T> {
+            if self.once.is_completed() {
+                unsafe { &(*self.inner.get()) }.as_ref()
+            } else {
+                None
+            }
+        }
+
+        pub fn set(&self, value: T) -> Result<(), T> {
+            if self.once.is_completed() {
+                return Err(value);
+            }
+            let mut value = Some(value);
+            self.once.call_once(|| {
+                let inner = unsafe { &mut (*self.inner.get()) };
+                debug_assert!(std::mem::replace(inner, value.take()).is_none());
+            });
+
+            match value {
+                None => Ok(()),
+                Some(value) => {
+                    debug_assert!(self.once.is_completed());
+                    Err(value)
+                }
+            }
+        }
     }
 }
 
@@ -37,12 +87,33 @@ impl<T> OnceCell<T> {
 mod tests {
     use super::*;
     #[test]
-    fn it_works() {
-        let once: OnceCell<String> = OnceCell::new();
+    fn unsync_works() {
+        let once: unsync::OnceCell<String> = unsync::OnceCell::new();
         assert!(once.get().is_none());
         assert!(once.set(String::new()).is_ok());
         assert!(once.set(String::new()).is_err());
         assert!(once.get().is_some());
         assert!(once.get().is_some());
+    }
+
+    #[test]
+    fn sync_works() {
+        use std::sync::Arc;
+
+        let once = Arc::new(sync::OnceCell::new());
+
+        let one = Arc::clone(&once);
+        std::thread::spawn(move || {
+            println!("{:?}", one.set(String::from("Hello")));
+        });
+
+        let two = Arc::clone(&once);
+        std::thread::spawn(move || {
+            println!("{:?}", two.set(String::from("World")));
+        });
+
+        std::thread::sleep(std::time::Duration::from_millis(10));
+
+        println!("{:?}", once.get());
     }
 }
